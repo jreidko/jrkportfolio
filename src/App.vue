93f25cf369e5ebd9e07a05eb35ef0e83a2e53d2a
Jason Koerner <jreidko@gmail.com>
2021-02-02 17:51:43 -0600

    Some data related work

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld class="" msg="Welcome to Your Vue.js App" />
+  <div class="bg-gray-700">
+    <h1>Jason Koerner</h1>
+    <h2>Product & Experience Design</h2>
+    <p>
+      After beginning my career in advertising as a traditional art director i
+      was slowly drawn to the blinking lights of the internet and interactive
+      media. Since that time i have started my own interactive design agency,
+      Schema Studios and worked at numerous interactive and advertising
+      agencies. I have worked on interactive projects for Nike, McDonald’s,
+      MSNBC, American Airlines, Discover Card, Anheuser-Busch, State Farm, Dow,
+      Sharpie, Boeing, KFC and many others.
+    </p>
+    <resume></resume>
   </div>
 </template>
 
 <script>
 import "tailwindcss/tailwind.css";
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Resume from "./components/Resume.vue";
 export default {
-  name: "App",
   components: {
-    HelloWorld,
+    Resume,
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Resume.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Resume</h1>
+    <p class="bg-gray-400">Stuff here</p>
+    <pre>{{ this.jobs }}</pre>
+    <pre>{{ this.awards }}</pre>
+  </div>
+</template>
+
+<script>
+import resume from "@/assets/resume.json";
+export default {
+  data() {
+    return {
+      jobs: resume.jobs,
+      awards: resume.awards,
+    };
+  },
+};
+</script>
